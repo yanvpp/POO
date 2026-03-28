9c76f4abf2a3cf4c10d58c94c190c2066bfbb4ff
Yan Vitor Pires Pereira <yanvitor.pp@gmail.com>
2026-03-28 08:40:31 -0300

    finalizando exercício 5

--- lab_03/exercicio_05/app/src/main/java/engtelecom/poo/Robo.java
+++ lab_03/exercicio_05/app/src/main/java/engtelecom/poo/Robo.java
@@ -4,13 +4,15 @@ public class Robo {
     private String nome;
     private long numSerie;
     private static long totalRobos = 0;
+    private double x;
+    private double y;
 
     public Robo(String nome) {
         this.nome = nome;
         this.numSerie = totalRobos + 1;
     }
 
-    public long getNumSerie(){
+    public long getNumSerie() {
         return numSerie;
     }
 
@@ -21,5 +23,18 @@ public class Robo {
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
+
+    public String getPosicao() {
+        return String.format("X: %.2f\tY: %.2f", x, y);
+    }
+
+    public void mover(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s\nSerial number: %d\nCoordinates: ", nome, numSerie) + toString();
+    }
 }
