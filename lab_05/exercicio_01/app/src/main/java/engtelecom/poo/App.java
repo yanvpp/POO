a42f34248ff8998f60b007039c9ae9ad7263e24b
Yan Vitor Pires Pereira <yanvitor.pp@gmail.com>
2026-04-29 11:06:36 -0300

    projeto para o exercicio 1

--- lab_05/exercicio_01/app/src/main/java/engtelecom/poo/App.java
+++ lab_05/exercicio_01/app/src/main/java/engtelecom/poo/App.java
@@ -4,11 +4,7 @@
 package engtelecom.poo;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        
     }
 }
