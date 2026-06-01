0470491dd8bdf7e30c5ea218d2d987ce05ffa911
Yan Pereira <yanvitor.pp@gmail.com>
2026-06-01 09:23:31 -0300

    main

--- aula_22/app/src/main/java/engtelecom/poo/App.java
+++ aula_22/app/src/main/java/engtelecom/poo/App.java
@@ -1,7 +1,47 @@
 package engtelecom.poo;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import engtelecom.poo.baralhoFrances.Carta;
+import engtelecom.poo.diasDaSemana.DiasDaSemana;
+import engtelecom.poo.diasDaSemana.Disciplina;
+
 public class App {
     public static void main(String[] args) {
 
+        String sigla = IO.readln("Entre com a sigla: ");
+
+        int dia1 = Integer.parseInt(IO.readln("Entre com o 1º dia: "));
+        DiasDaSemana d1 = DiasDaSemana.getByCode(dia1);
+
+        int dia2 = Integer.parseInt(IO.readln("Entre com o 2º dia: "));
+        DiasDaSemana d2 = DiasDaSemana.getByCode(dia2);
+
+        DiasDaSemana[] dias = {d1, d2};
+
+        Disciplina poo = new Disciplina(sigla, dias);
+
+        System.out.println(poo);
+
+
+
+        // exemplo dos planetas
+        // int posicao = Integer.parseInt(IO.readln("Entre com a posição do planeta: "));
+        
+        // Planetas planeta = Planetas.getByPosition(posicao);
+
+        System.out.println(Planetas.getByPosition(Integer.parseInt(IO.readln("Entre com a posição do planeta: "))));
+    
+    
+        // exemplo do baralho
+
+        Deque<Carta> baralho = new ArrayDeque<>();
+
+        // for (Naipes n){
+        //     for (Valores v){
+        //         carta = new Carta(n, v)
+        //     }
+        // }
     }
 }
